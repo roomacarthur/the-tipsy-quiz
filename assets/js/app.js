b6acf74570f7e18b0110408ca39cdceb12835f77
Roomacarthur <roomacarthur@gmail.com>
2021-05-14 21:11:22 +0100

    js import from json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-console.log("hello")
+//fetch data from question and answer JSON file. 
+//Coded with help from w3 schools.(https://www.w3schools.com/xml/xml_http.asp)
+const questionAndAnswers = new XMLHttpRequest();
+questionAndAnswers.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let response = JSON.parse(questionAndAnswers.responseText);
+        let questions = response.questions;
+        console.log(questions[1].question)
+    }
+};
+questionAndAnswers.open("GET", "./assets/js/questions-answers/qa.JSON", true);
+questionAndAnswers.send();
+
+const questionText = document.getElementById('question');
+
+function getNewQuestion() {
+    
+}
