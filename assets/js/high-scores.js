3ec7495e6591c18e246091b2b6a1602e6781b7f0
Roomacarthur <roomacarthur@gmail.com>
2021-06-01 16:45:49 +0100

    updated comments in high-scores.js

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,10 +1,11 @@
 const highScoresRank = document.getElementById("high-score-ranks");
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-//access the JSON object saved in high-scores.js, create a Li that includes the data from the JSON object to show in the high scores table. ---- Brian Design's Video helped with this section of code.(Credit #1 in README.)
+//access the JSON object saved in high-scores.js, create an array with a Li that includes the data from the JSON object to show in the high scores table. ---- Brian Design's Video helped with this section of code.(Credit #1 in README.)
 highScoresRank.innerHTML = highScores.map(function setScore(score){
     return `<li class="high-score">${score.name} <span class="score-count">${score.score}</span></li>`;
 }).join('');
+//end credit.
 
 //Clear high-scores from local storage, alert the user when clicked to confirm the action. Called via onclick in high-scores.html
 function clearScores() {
