7367dc57829f73d7c0916693ce569c57e7d6ca4f
Roomacarthur <roomacarthur@gmail.com>
2021-06-03 12:58:26 +0100

    updated cache address in sw.js

--- assets/pwa/sw.js
+++ assets/pwa/sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('sw-cache').then(function(cache) {
         // cache any static files that make up the application shell
-        return cache.add('index.html');
+        return cache.add('../../index.html');
       })
     );
   });
